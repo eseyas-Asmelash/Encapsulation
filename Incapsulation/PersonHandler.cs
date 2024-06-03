f952b2da5d0f644c27b16fe804ed8ade99c24a73
eseyas.asmelash <ezaweld@gmail.com>
2024-06-03 10:16:40 +0200

    Edited Encpsuation program class

--- Incapsulation/PersonHandler.cs
+++ Incapsulation/PersonHandler.cs
@@ -67,22 +67,17 @@ class PersonHandler
 
         return person;
     }
-    public void ReadPersonValues()
+    public void ReadPersonValues(int age, string fName, string lName, int weight)
     {
-        Console.WriteLine("Enter the Age of the Person");
-        int.TryParse(Console.ReadLine(), out int _age);
+        try
+        {
+            CreatePerson(age, fName, lName, weight);
+        }
+        catch (ArgumentException ex)
+        {
 
-        Console.WriteLine("Enter the FirstName of the Person");
-        string _fName = Console.ReadLine()!;
-
-
-        Console.WriteLine("Enter the Last Name of the Person");
-        string _lName = Console.ReadLine()!;
-
-        Console.WriteLine("Enter the Weight of the Person");
-        int.TryParse(Console.ReadLine(), out int _weight);
-
-        CreatePerson(_age, _fName, _lName, _weight);
+            Console.WriteLine(ex.Message);
+        }
 
     }
 
--- Incapsulation/Program.cs
+++ Incapsulation/Program.cs
@@ -6,31 +6,32 @@ class Program
     {
          List<Person> persons = new List<Person>();
         PersonHandler person = new PersonHandler();
-        try
-        {
+        //try
+        //{
             
-            var p = person.CreatePerson(1, "an", "dem", 70);
-            var p1 = person.CreatePerson(23, "Junee", "roberts", 59);
-            var p2 = person.CreatePerson(23, "june", "mathson", 20);
-            var p3 = person.CreatePerson(34, "Simon", "Sammm", 23);
-            //persons.Add(person.CreatePerson(1, "alan", "dave", 70));
-            //persons.Add(person.CreatePerson(23, "Junee", "roberts", 59));
-            //persons.Add(person.CreatePerson(23, "june", "mathson", 20));
-            //persons.Add(person.CreatePerson(- 34, "Simon", "Sammm", 23));
-            persons.Add(p);
-            persons.Add(p1);
-            persons.Add(p3);
-            persons.Add(p2);
-        }
+            //var p = person.CreatePerson(1, "an", "dem", 70);
+            //var p1 = person.CreatePerson(23, "Junee", "roberts", 59);
+            //var p2 = person.CreatePerson(23, "june", "mathson", 20);
+            //var p3 = person.CreatePerson(34, "Simon", "Sammm", 23);
+        person.ReadPersonValues(1, "an", "dem", 70);
+        person.ReadPersonValues(23, "Junee", "roberts", 59);
+        person.CreatePerson(23, "june", "mathson", 20);
+        person.ReadPersonValues(34, "Simon", "Sammm", 23);
+        //persons.Add(person.CreatePerson(1, "alan", "dave", 70));
+        //persons.Add(person.CreatePerson(23, "Junee", "roberts", 59));
+        //persons.Add(person.CreatePerson(23, "june", "mathson", 20));
+        //persons.Add(person.CreatePerson(- 34, "Simon", "Sammm", 23));
 
-        catch (ArgumentException ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
+        //}
 
-        foreach (var p in persons)
+        //catch (ArgumentException ex)
+        //{
+        //    Console.WriteLine(ex.Message);
+        //}
+
+        foreach (var pe in persons)
         {
-            Console.WriteLine($"{p.Age} {p.FName} {p.LName} {p.Weight}");
+            Console.WriteLine($"{pe.Age} {pe.FName} {pe.LName} {pe.Weight}");
 
         }
 
